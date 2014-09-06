704622600c9a91799b52bbdc703da007e82c7200
icebox <albertosantini@gmail.com>
2014-09-06 13:28:18 +0200

    Fix a typo in rendering script.

--- scripts/renderScenes.py
+++ scripts/renderScenes.py
@@ -1,6 +1,6 @@
 # Render the scenes and create the final movie.
 # The scene are ordered: the final one is to compose the final output.
 
-for scene bpy.data.scenes:
+for scene in bpy.data.scenes:
     print("RENDERING SCENE: %s" % scene.name)
     bpy.ops.render.render(animation=True, scene=scene.name)
